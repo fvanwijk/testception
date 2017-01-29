cf12e3aae9b806a5c71f9e36d0b7bef5ff1cc8db
Frank van Wijk <f.e.vanwijk@gmail.com>
2017-01-29 17:11:25 +0100

    Update Karma config so that it uses new Phantom version

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['PhantomJS2'],
+    browsers: ['PhantomJS'],
     captureTimeout: 5000,
     singleRun: true,
     coverageReporter: {
