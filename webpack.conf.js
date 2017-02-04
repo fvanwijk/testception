a965a845ac9289c356a9959ac80f43f13e863d96
Frank van Wijk <f.e.vanwijk@gmail.com>
2017-02-04 10:24:06 +0100

    Create Webpack config for development and production build

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,37 +1,53 @@
 const path = require('path');
 const webpack = require('webpack');
-
-module.exports = {
-  entry: {
-    'testception': __dirname + '/src/testception.js',
-    'testception.min': __dirname + '/src/testception.js'
-  },
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/dist',
-    filename: '[name].js',
-    library: 'expectMatcher', // expectMatcher is the only default export. We use add-module-exports plugin to export this function as default
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: /(node_modules)/
-      }
+const params = {
+  production: {
+    output: 'testception.min',
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        include: /\.min\.js$/,
+        compress: {warnings: false}
+      })
     ]
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      include: /\.min\.js$/,
-      compress: { warnings: false }
-    })
-  ],
-  resolve: {
-    root: path.resolve('./src'),
-    extensions: ['', '.js']
+  development: {
+    output: 'testception',
+    devtool: 'inline-source-map'
   }
 };
+
+function getConfig(env) {
+  return {
+    entry: {
+      [params[env].output]: __dirname + '/src/testception.js'
+    },
+    devtool: params[env].devtool,
+    output: {
+      path: __dirname + '/dist',
+      filename: '[name].js',
+      library: 'expectMatcher', // expectMatcher is the only default export. We use add-module-exports plugin to export this function as default
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel',
+          exclude: /(node_modules)/
+        }
+      ]
+    },
+    plugins: params[env].plugins,
+    resolve: {
+      root: path.resolve('./src'),
+      extensions: ['', '.bjs']
+    }
+  };
+}
+
+module.exports = [
+  getConfig('development'),
+  getConfig('production')
+];
