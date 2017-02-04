fd07d1f8e06199c59e1d6b7a97bbeb31a637e5e9
Frank van Wijk <f.e.vanwijk@gmail.com>
2017-02-05 00:32:41 +0100

    Relative paths in Webpack config

--- webpack.conf.js
+++ webpack.conf.js
@@ -21,11 +21,11 @@ const params = {
 function getConfig(env) {
   return {
     entry: {
-      [params[env].output]: path.resolve(__dirname, '/src/testception.js')
+      [params[env].output]: path.join(__dirname, '/src/testception.js')
     },
     devtool: params[env].devtool,
     output: {
-      path: path.resolve(__dirname, '/dist'),
+      path: path.join(__dirname, '/dist'),
       filename: '[name].js',
       library: 'expectMatcher', // expectMatcher is the only default export. We use add-module-exports plugin to export this function as default
       libraryTarget: 'umd',
